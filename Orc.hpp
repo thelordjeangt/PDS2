ee9e577855a7b411940cf021102a53adc4f1a99f
JeanGT <47991251+thelordjeangt@users.noreply.github.com>
2024-06-09 17:06:17 -0300

    Create Orc.hpp

---
+++ Orc.hpp
@@ -0,0 +1,12 @@
+#ifndef ORC_HPP
+#define ORC_HPP
+
+#include "Inimigo.hpp"
+// classe orc herda da classe Inimigo
+class Orc : public Inimigo {
+public:
+    // Construtor da classe Orc
+    Orc(int posicao);
+};
+
+#endif

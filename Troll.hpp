f22f87f84a9878e0c1169870b7f001fc19196f7a
JeanGT <47991251+thelordjeangt@users.noreply.github.com>
2024-06-09 17:19:53 -0300

    Create Troll.hpp

---
+++ Troll.hpp
@@ -0,0 +1,12 @@
+#ifndef TROLL_HPP
+#define TROLL_HPP
+
+#include "Inimigo.hpp"
+// classe troll herda da classe Inimigo
+class Troll : public Inimigo {
+public:
+    // Construtor da classe troll
+    Troll(int posicao);
+};
+
+#endif

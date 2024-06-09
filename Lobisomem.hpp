ae9eba34dab2251e5bcd6bd430f19dd9334423c5
JeanGT <47991251+thelordjeangt@users.noreply.github.com>
2024-06-09 17:13:59 -0300

    Create Lobisomem.hpp

---
+++ Lobisomem.hpp
@@ -0,0 +1,12 @@
+#ifndef LOBISOMEM_HPP
+#define LOBISOMEM_HPP
+
+#include "Inimigo.hpp"
+// classe lobisomem herda da classe Inimigo
+class Lobisomem : public Inimigo {
+public:
+    // Construtor da classe Lobisomem
+    Lobisomem(int posicao);
+};
+
+#endif

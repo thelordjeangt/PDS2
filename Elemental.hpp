614716c7e2fbcf029703b51234537d71af5bb325
JeanGT <47991251+thelordjeangt@users.noreply.github.com>
2024-06-09 17:22:46 -0300

    Create Elemental.hpp

---
+++ Elemental.hpp
@@ -0,0 +1,12 @@
+#ifndef ELEMENTAL_HPP
+#define ELEMENTAL_HPP
+
+#include "Inimigo.hpp"
+// classe elemental herda da classe Inimigo
+class Elemental : public Inimigo {
+public:
+    // Construtor da classe Elemental
+    Elemental(int posicao);
+};
+
+#endif

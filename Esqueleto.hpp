ea7b99e552d26bf41502516802b3a948a1822eba
JeanGT <47991251+thelordjeangt@users.noreply.github.com>
2024-06-09 16:37:21 -0300

    Create Esqueleto.hpp

---
+++ Esqueleto.hpp
@@ -0,0 +1,12 @@
+#ifndef ESQUELETO_HPP
+#define ESQUELETO_HPP
+
+#include "Inimigo.hpp"
+// classe esqueleto herda da classe Inimigo
+class Esqueleto : public Inimigo {
+public:
+    // Construtor da classe Esqueleto
+    Esqueleto(int posicao);
+};
+
+#endif

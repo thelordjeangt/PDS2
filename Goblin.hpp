c06ad30dc7dddcaf45fb542741a52e6f64920f28
JeanGT <47991251+thelordjeangt@users.noreply.github.com>
2024-06-09 17:19:02 -0300

    Create Goblin.hpp

---
+++ Goblin.hpp
@@ -0,0 +1,12 @@
+#ifndef GOBLIN_HPP
+#define GOBLIN_HPP
+
+#include "Inimigo.hpp"
+// classe goblin herda da classe Inimigo
+class Goblin : public Inimigo {
+public:
+    // Construtor da classe Goblin
+    Goblin(int posicao);
+};
+
+#endif

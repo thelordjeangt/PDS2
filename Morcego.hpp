3e42ca1509ce95e9696b120352ce19dfcf075d47
JeanGT <47991251+thelordjeangt@users.noreply.github.com>
2024-06-09 17:11:48 -0300

    Create Morcego.hpp

---
+++ Morcego.hpp
@@ -0,0 +1,12 @@
+#ifndef MORCEGO_HPP
+#define MORCEGO_HPP
+
+#include "Inimigo.hpp"
+// classe morcego herda da classe Inimigo
+class Morcego : public Inimigo {
+public:
+    // Construtor da classe Morcego
+    Morcego(int posicao);
+};
+
+#endif

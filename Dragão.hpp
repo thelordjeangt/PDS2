babbfaa88cc04df924d31e2e294b47d0b83988e1
JeanGT <47991251+thelordjeangt@users.noreply.github.com>
2024-06-09 17:25:53 -0300

    Create Dragão.hpp

---
+++ Dragão.hpp
@@ -0,0 +1,12 @@
+#ifndef DRAGÃO_HPP
+#define DRAGÃO_HPP
+
+#include "Inimigo.hpp"
+// classe dragão herda da classe Inimigo
+class Dragão : public Inimigo {
+public:
+    // Construtor da classe Dragão
+    Dragão(int posicao);
+};
+
+#endif

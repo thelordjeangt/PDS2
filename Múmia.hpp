a4f881af70f35945de850da0e776d1c9bc6abf33
JeanGT <47991251+thelordjeangt@users.noreply.github.com>
2024-06-09 17:25:02 -0300

    Create Múmia.hpp

---
+++ Múmia.hpp
@@ -0,0 +1,12 @@
+#ifndef MÚMIA_HPP
+#define MÚMIA_HPP
+
+#include "Inimigo.hpp"
+// classe múmia herda da classe Inimigo
+class Múmia : public Inimigo {
+public:
+    // Construtor da classe Múmia
+    Múmia(int posicao);
+};
+
+#endif

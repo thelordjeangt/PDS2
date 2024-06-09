e528b99902a2b22dc543a649bcd51aaf5e48f909
JeanGT <47991251+thelordjeangt@users.noreply.github.com>
2024-06-09 17:21:19 -0300

    Create Gárgula.hpp

---
+++ Gárgula.hpp
@@ -0,0 +1,12 @@
+#ifndef GÁRGULA_HPP
+#define GÁRGULA_HPP
+
+#include "Inimigo.hpp"
+// classe gárgula herda da classe Inimigo
+class Gárgula : public Inimigo {
+public:
+    // Construtor da classe Gárgula
+    Gárgula(int posicao);
+};
+
+#endif

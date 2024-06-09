7007963a0eced079c44c43ad770fc8f2e090066f
JeanGT <47991251+thelordjeangt@users.noreply.github.com>
2024-06-09 17:05:11 -0300

    Create Zumbi.hpp

---
+++ Zumbi.hpp
@@ -0,0 +1,12 @@
+#ifndef ZUMBI_HPP
+#define ZUMBI_HPP
+
+#include "Inimigo.hpp"
+// classe zumbi herda da classe Inimigo
+class Zumbi : public Inimigo {
+public:
+    // Construtor da classe Zumbi
+    Zumbi(int posicao);
+};
+
+#endif
